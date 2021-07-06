01945ef7f06f895199fa83e43963b0be17b88945
Prabhu <psanthanam@ethz.ch>
2021-07-06 12:53:44 +0000

    Update index.js with initialising push instance. Need to be done after removing VersionSwitch.js.

--- index.js
+++ index.js
@@ -6,5 +6,15 @@ import 'react-native-gesture-handler'
 import { AppRegistry } from 'react-native'
 import App from './App/Containers/App'
 import { name as appName } from './app.json'
+import AppConfig from './App/Config/AppConfig'
+import PushNotifications from './App/Utils/PushNotifications'
+
+PushNotifications
+      .getInstance()
+      .init(
+        AppConfig.config.dev.purgeStoreAtStartup,
+        AppConfig.config.serverSync.androidSenderId,
+        AppConfig.config.startup.automaticallyRequestPushPermissions
+      )
 
 AppRegistry.registerComponent(appName, () => App)
