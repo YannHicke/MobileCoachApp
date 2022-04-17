2bbae0fcb287d270c1475cec9c6cc25614a47d88
Elena Svetoslavova Stoeva <ess243@cornell.edu>
2022-04-17 10:26:23 -0400

    Revert changes to Dashboard
    This reverts commit d9cd93b49dfe68563152642358b0017030745cec.

--- App/Containers/Dashboard/Dashboard.js
+++ App/Containers/Dashboard/Dashboard.js
@@ -15,7 +15,7 @@ import ParsedText from 'react-native-parsed-text';
 import Collapsible from 'react-native-collapsible';
 import propTypes from 'prop-types';
 import { Icon, Card, Button } from 'react-native-elements';
-import Star from './Star';
+import Star from "./Star";
 import EmptyStar from './empty-star.png';
 
 // import { CommonActions as NavigationActions } from '@react-navigation/native';
@@ -39,28 +39,15 @@ const ConnectedComponent = connectToStore(Component)
 connect(mapStateToProps, mapDispatchToProps)(Component)
 */
 
-export default class Dashboard extends Component {
-
-  constructor() {
-    super();
-    this.state = {
-      activeSection: false,
-      collapsed: true,
-      rating: 0,
-      star_states: [1, 1, 1, 0, 0, 0, 0], // 1 represents filled-in star, 0 represents empty star
-      num_points: 0,
-      num_interactions: 79, // the total number of points an user collects
-    };
-  }
-
-  // state = {
-  //   activeSection: false,
-  //   collapsed: true,
-  //   rating: 0,
-  //   star_states: [1,1,1,0,0,0,0], // 1 represents filled-in star, 0 represents empty star
-  //   num_points: 0,
-  //   num_interactions: 79 // the total number of points an user collects
-  // };
+class Dashboard extends Component {
+  state = {
+    activeSection: false,
+    collapsed: true,
+    rating: 0,
+    star_states: [1,1,1,0,0,0,0], // 1 represents filled-in star, 0 represents empty star
+    num_points: 0,
+    num_interactions: 79 // the total number of points an user collects
+  };
 
   toggleExpanded = () => {
     this.setState({ collapsed: !this.state.collapsed });
@@ -76,12 +63,12 @@ export default class Dashboard extends Component {
       <PMNavigationBar title={title} props={props} rightButton={<View />} />
     );
   }
-
+  
   /** [count_on] counts the number of stars that are filled in. */
   count_on() {
     var num_ones = 0;
-    this.state.star_states.forEach((v) => v === 1 && num_ones++);
-    return num_ones;
+    this.state.star_states.forEach((v) => (v === 1 && num_ones++));
+    return num_ones
   }
 
   /** [switch] fills a new star in (is called when a user finishes all daily tasks). */
@@ -96,7 +83,8 @@ export default class Dashboard extends Component {
   is_on(pos) {
     if (this.state.star_states[pos] == 1) {
       return true;
-    } else {
+    }
+    else {
       return false;
     }
   }
@@ -111,61 +99,80 @@ export default class Dashboard extends Component {
     this.server_retrieve();
     const { openURL } = this.props.route.params.screenProps;
     var paths = [];
-
+    
     for (var i = 0; i < 7; i++) {
       if (this.is_on(i)) {
-        paths.push({ key: i, value: require('./filled-star.png') });
-      } else {
-        paths.push({ key: i, value: require('./empty-star.png') });
+        paths.push({key:i, value:require('./filled-star.png')});
+      }
+      else {
+        paths.push({key:i, value:require('./empty-star.png')});
       }
     }
 
     return (
       <ScrollView style={styles.content} indicatorStyle="white">
-        <View style={{ justifyContent: 'space-between' }}>
+        <View style={{justifyContent: 'space-between'}}>
           {this.renderNavigationbar(this.props)}
           <Card
             title={I18n.t('Dashboard.dashboardTitle')}
             titleStyle={styles.cardTitle}
             containerStyle={{ marginBottom: 15 }}>
             <Text>Welcome to MobileCoach!</Text>
-            <Text />
-            <Text>
-              Below, you will find your progress. Each star corresponds to task
-              completion for a single day.
-            </Text>
+            <Text/>
+            <Text>Below, you will find your progress. Each star corresponds to task completion for a single day.</Text>
           </Card>
-          <Text />
-          <View style={{ flexDirection: 'row', flex: 2 }}>
-            <Image style={styles.stretch} source={paths[0].value} />
-            <Image style={styles.stretch} source={paths[1].value} />
-            <Image style={styles.stretch} source={paths[2].value} />
-            <Image style={styles.stretch} source={paths[3].value} />
-            <Image style={styles.stretch} source={paths[4].value} />
-            <Image style={styles.stretch} source={paths[5].value} />
-            <Image style={styles.stretch} source={paths[6].value} />
+          <Text/>
+          <View style={{flexDirection:'row', flex:2}}>
+              <Image
+                style={styles.stretch}
+                source={paths[0].value}
+              />
+              <Image
+                style={styles.stretch}
+                source={paths[1].value}
+              />
+              <Image
+                style={styles.stretch}
+                source={paths[2].value}
+              />
+              <Image
+                style={styles.stretch}
+                source={paths[3].value}
+              />
+              <Image
+                style={styles.stretch}
+                source={paths[4].value}
+              />
+              <Image
+                style={styles.stretch}
+                source={paths[5].value}
+              />
+              <Image
+                style={styles.stretch}
+                source={paths[6].value}
+              />
           </View>
           <View>
             <Image
-              style={styles.circle}
-              source={require('./blue-circle.png')}
+             style={styles.circle}
+             source={require('./blue-circle.png')}
             />
             <Text style={styles.numInteraction}>
               {this.state.num_interactions}
             </Text>
           </View>
         </View>
-        <Text />
+        <Text/>
         {/*<Text style={{textAlignVertical: "center", textAlign: "center", color: "blue"}}>
           You have a total of {this.state.num_points} points!
         </Text>
     */}
       </ScrollView>
-    );
+    )
   }
 }
 
-// export default Dashboard;
+export default Dashboard;
 
 /** the stylesheet of the Dashboard page */
 const styles = StyleSheet.create({
@@ -194,15 +201,15 @@ const styles = StyleSheet.create({
     height: 200,
     left: 100,
     marginTop: 20,
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   numInteraction: {
-    textAlign: 'center',
-    color: 'white',
-    fontWeight: 'bold',
+    textAlign: "center", 
+    color: "white",
+    fontWeight: 'bold',  
     fontSize: 50,
-    left: 0,
-    bottom: 130,
+    left:0,
+    bottom: 130
   },
   cardTitle: {
     textAlign: 'left',
@@ -211,4 +218,5 @@ const styles = StyleSheet.create({
   content: {
     flex: 1,
   },
+
 });
--- App/Containers/_tests_/Dashboard.test.js
+++ App/Containers/_tests_/Dashboard.test.js
@@ -1,39 +1,15 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { Dashboard } from "../Dashboard/Dashboard";
+import Dashboard from "../Dashboard/Dashboard";
 
-// const TestD = require("../Dashboard/Dashboard");
 
-// const TestD = require("../Dashboard/TestD");
-import TestD from "../Dashboard/TestD";
 
-describe("TestD", () => {
-  const testD = new TestD();
-  console.log("in testD");
+describe("Tests for is_on()", () => {
 
-  test("defines is_on()", () => {
-    expect(typeof testD.is_on).toBe("function");
+  const dashboard = new Dashboard();
+
+  test('check for True output', () => {
+    expect(dashboard.is_on(0)).toBe(1);
   });
+
 });
-
-// describe("Tests for is_on()", () => {
-
-//   const dashboard = new Dashboard();
-//   console.log(dashboard);
-
-//   // test('check for True output', () => {
-//   //   expect(is_on(0)).toBe(1);
-//   // });
-
-// });
-
-// describe("Tests for is_on()", () => {
-
-//   const test = new TestD();
-//   console.log(test);
-
-//   // test('check for True output', () => {
-//   //   expect(is_on(0)).toBe(1);
-//   // });
-
-// });
