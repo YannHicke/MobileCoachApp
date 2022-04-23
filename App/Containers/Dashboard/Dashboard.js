e8e597a2782666c8b35e519bb3ccbb557be4aaf5
Ashley <yy346@cornell.edu>
2022-04-23 16:27:40 -0400

    static visualization

--- App/Containers/Dashboard/Dashboard.js
+++ App/Containers/Dashboard/Dashboard.js
@@ -17,6 +17,7 @@ import propTypes from 'prop-types';
 import { Icon, Card, Button } from 'react-native-elements';
 import Star from "./Star";
 import EmptyStar from './empty-star.png';
+import { TextInput} from 'react-native';
 
 // import { CommonActions as NavigationActions } from '@react-navigation/native';
 import { Colors } from '../../Themes/';
@@ -45,8 +46,11 @@ class Dashboard extends Component {
     collapsed: true,
     rating: 0,
     star_states: [1,1,1,0,0,0,0], // 1 represents filled-in star, 0 represents empty star
-    num_points: 0,
-    num_interactions: 79 // the total number of points an user collects
+    num_points: 0, // the total number of points an user collects
+    num_interactions: '80%',
+    last_week: 5,
+    highest: 7,
+    plans: ["Watch a lecture", "Reading 1", "completing quiz 1"] 
   };
 
   toggleExpanded = () => {
@@ -109,10 +113,12 @@ class Dashboard extends Component {
       }
     }
 
+
     return (
       <ScrollView style={styles.content} indicatorStyle="white">
         <View style={{justifyContent: 'space-between'}}>
           {this.renderNavigationbar(this.props)}
+          {/*
           <Card
             title={I18n.t('Dashboard.dashboardTitle')}
             titleStyle={styles.cardTitle}
@@ -121,6 +127,7 @@ class Dashboard extends Component {
             <Text/>
             <Text>Below, you will find your progress. Each star corresponds to task completion for a single day.</Text>
           </Card>
+    */}
           <Text/>
           <View style={{flexDirection:'row', flex:2}}>
               <Image
@@ -163,10 +170,45 @@ class Dashboard extends Component {
           </View>
         </View>
         <Text/>
-        {/*<Text style={{textAlignVertical: "center", textAlign: "center", color: "blue"}}>
+        {/*
+        <View>
+        <Text style={styles.label}>Participant ID: </Text>
+        <TextInput style={styles.input}/>
+        <Button
+        title="Submit"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
+      </View>
+        <Text style={{textAlignVertical: "center", textAlign: "center", color: "blue"}}>
           You have a total of {this.state.num_points} points!
         </Text>
     */}
+      <View style={styles.subsection}>
+        <Text style={styles.heading}>
+            Your Plan
+        </Text>
+        <Text style={styles.bullet}>
+          {this.state.plans[0]}
+        </Text>
+        <Text style={styles.bullet}>
+          {this.state.plans[1]}
+        </Text>
+        <Text style={styles.bullet}>
+          {this.state.plans[2]}
+        </Text>
+      </View>
+
+      <View style={styles.subsection}>
+        <Text style={styles.heading}>
+            Your Past Accomplishments
+        </Text>
+        <Text style={styles.bullet}>
+          Last week: {this.state.last_week} stars
+        </Text>
+        <Text  style={styles.bullet}>
+          Highest: {this.state.highest} stars
+        </Text>
+      </View>
       </ScrollView>
     )
   }
@@ -218,5 +260,29 @@ const styles = StyleSheet.create({
   content: {
     flex: 1,
   },
-
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  label: {
+    marginLeft: 12
+  },
+  heading: {
+    marginTop: 5,
+    marginLeft: 12,
+    textAlign: "left",
+    fontSize: 28,
+    fontWeight: 'bold'
+  },
+  bullet: {
+    marginTop: 5,
+    marginLeft: 12,
+    textAlign: "left",
+    fontSize: 18
+  },
+  subsection: {
+    marginBottom: 20
+  }
 });
