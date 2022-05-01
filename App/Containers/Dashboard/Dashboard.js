36f165c6b974f77e54ea96d44a30006b17f9a336
Rayan S Wali <rsw244@cornell.edu>
2022-05-01 19:00:57 -0400

    Resize components and add documentation

--- App/Containers/Dashboard/Dashboard.js
+++ App/Containers/Dashboard/Dashboard.js
@@ -14,7 +14,6 @@ import ParsedText from 'react-native-parsed-text';
 import Collapsible from 'react-native-collapsible';
 import { Icon, Card, Button } from 'react-native-elements';
 // import { CircularProgressbar } from 'react-circular-progressbar';
-// import { CommonActions as NavigationActions } from '@react-navigation/native';
 import { Colors } from '../../Themes/';
 import PMNavigationBar from '../../Components/Navbar';
 import I18n from '../../I18n/I18n';
@@ -23,72 +22,57 @@ import Log from '../../Utils/Log';
 import { requestData } from '../../Sagas/ServerSyncSagas';
 const log = new Log('Containers/Dashboard');
 
-/*
-const mapStateToProps = (state, ownProps) => ({})
-const mapDispatchToProps = {}
-
-const connectToStore = connect(mapStateToProps, mapDispatchToProps)
-const ConnectedComponent = connectToStore(Component)
-
-connect(mapStateToProps, mapDispatchToProps)(Component)
-*/
 
 class Dashboard extends Component {
   state = {
     activeSection: false,
     collapsed: true,
-    rating: 0,
-    star_states: [0, 0, 0, 0, 0, 0, 0], // 1 represents filled-in star, 0 represents empty star
-    stars: 0,
-    num_points: 0, // the total number of points an user collects
-    last_week: 0,
-    highest: 0,
-    totTasks: 0,
-    task1Complete: 0,
-    task2Complete: 0,
-    task3Complete: 0,
-    task4Complete: 0,
-    task5Complete: 0,
-    task1: "",
-    task2: "",
-    task3: "",
-    task4: "",
-    task5: "",
-    proportion_tasks_complete: 0
+    starStates: [0,0,0,0,0,0,0], // 1 represents filled-in star, 0 represents empty star
+    stars: 0, // the number of stars from the current week
+    numPoints: 0, // the total number of points an user collects
+    lastWeek: 0, // the number of stars from the last week
+    highest: 0, // the maximum stars from all weeks
+    course: "", // the course name the user enters
+    totTasks: 0, // the total number of tasks an user enters
+    dynamicTasks: [], // the total list of tasks an user enters
+    dynamicTasksCompletion: [], // completion status for each task an user enters
+    proportionTasksComplete: 0 // the proportion of tasks an user enters that are complete
   };
 
+  /* first time data is requested */
   componentDidMount() {
     requestData()
     .then(data => {
       if (parseInt(data.numericData[8]) != 0) {
-        this.setState({'stars': data.numericData[0], 'highest': data.numericData[1], 'last_week': data.numericData[2],
-          'task1Complete': data.numericData[3], 'task2Complete': data.numericData[4], 'task3Complete': data.numericData[5], 
-          'task4Complete': data.numericData[6], 'task5Complete': data.numericData[7], 'totTasks': data.numericData[8], 'task1': data.textData[0], 'task2': data.textData[1], 'task3': data.textData[2], 'task4': data.textData[3], 'task5': data.textData[4],
-          'proportion_tasks_complete': (parseInt(data.numericData[3]) + parseInt(data.numericData[4]) + parseInt(data.numericData[5]) + parseInt(data.numericData[6]) + parseInt(data.numericData[7])) / parseInt(data.numericData[8]) * 100});
+        this.setState({'stars': data.numericData[0], 'highest': data.numericData[1], 'lastWeek': data.numericData[2], 'totTasks': data.numericData[8], 'course': data.textData[5],
+          'proportionTasksComplete': (parseInt(data.numericData[3]) + parseInt(data.numericData[4]) + parseInt(data.numericData[5]) + parseInt(data.numericData[6]) + parseInt(data.numericData[7])) / parseInt(data.numericData[8]) * 100});
       }
       else {
-        this.setState({'stars': data.numericData[0], 'highest': data.numericData[1], 'last_week': data.numericData[2],
-          'task1Complete': data.numericData[3], 'task2Complete': data.numericData[4], 'task3Complete': data.numericData[5], 
-          'task4Complete': data.numericData[6], 'task5Complete': data.numericData[7], 'totTasks': data.numericData[8], 'task1': data.textData[0], 'task2': data.textData[1], 'task3': data.textData[2], 'task4': data.textData[3], 'task5': data.textData[4],
-          'proportion_tasks_complete': 0});
+        this.setState({'stars': data.numericData[0], 'highest': data.numericData[1], 'lastWeek': data.numericData[2], 'totTasks': data.numericData[8], 'course': data.textData[5],
+          'proportionTasksComplete': 0});
+      }
+      for (let i = 0; i < data.numericData[8]; i++) {
+        this.state.dynamicTasks[i] = data.textData[i];
+        this.state.dynamicTasksCompletion[i] = data.numericData[i+3];
       }
     });
   }
 
+  /* automatically updates state as data changes */
   componentDidUpdate() {
     requestData()
     .then(data => {
       if (parseInt(data.numericData[8]) != 0) {
-        this.setState({'stars': data.numericData[0], 'highest': data.numericData[1], 'last_week': data.numericData[2],
-          'task1Complete': data.numericData[3], 'task2Complete': data.numericData[4], 'task3Complete': data.numericData[5], 
-          'task4Complete': data.numericData[6], 'task5Complete': data.numericData[7], 'totTasks': data.numericData[8], 'task1': data.textData[0], 'task2': data.textData[1], 'task3': data.textData[2], 'task4': data.textData[3], 'task5': data.textData[4],
-          'proportion_tasks_complete': (parseInt(data.numericData[3]) + parseInt(data.numericData[4]) + parseInt(data.numericData[5]) + parseInt(data.numericData[6]) + parseInt(data.numericData[7])) / parseInt(data.numericData[8]) * 100});
+        this.setState({'stars': data.numericData[0], 'highest': data.numericData[1], 'lastWeek': data.numericData[2], 'totTasks': data.numericData[8], 'course': data.textData[5],
+          'proportionTasksComplete': (parseInt(data.numericData[3]) + parseInt(data.numericData[4]) + parseInt(data.numericData[5]) + parseInt(data.numericData[6]) + parseInt(data.numericData[7])) / parseInt(data.numericData[8]) * 100});
       }
       else {
-        this.setState({'stars': data.numericData[0], 'highest': data.numericData[1], 'last_week': data.numericData[2],
-          'task1Complete': data.numericData[3], 'task2Complete': data.numericData[4], 'task3Complete': data.numericData[5], 
-          'task4Complete': data.numericData[6], 'task5Complete': data.numericData[7], 'totTasks': data.numericData[8], 'task1': data.textData[0], 'task2': data.textData[1], 'task3': data.textData[2], 'task4': data.textData[3], 'task5': data.textData[4],
-          'proportion_tasks_complete': 0});
+        this.setState({'stars': data.numericData[0], 'highest': data.numericData[1], 'lastWeek': data.numericData[2], 'totTasks': data.numericData[8], 'course': data.textData[5],
+          'proportionTasksComplete': 0});
+      }
+      for (let i = 0; i < data.numericData[8]; i++) {
+        this.state.dynamicTasks[i] = data.textData[i];
+        this.state.dynamicTasksCompletion[i] = data.numericData[i+3];
       }
     });
   }
@@ -111,21 +95,21 @@ class Dashboard extends Component {
   /** [count_on] counts the number of stars that are filled in. */
   count_on() {
     var num_ones = 0;
-    this.state.star_states.forEach((v) => (v === 1 && num_ones++));
+    this.state.starStates.forEach((v) => (v === 1 && num_ones++));
     return num_ones;
   }
 
   /** [switch] fills a new star in (is called when a user finishes all daily tasks). */
   switch() {
     const { num_on } = this.count_on();
-    if (num_on < this.state.star_states.length) {
-      this.state.star_states[num_on] = 1;
+    if (num_on < this.state.starStates.length) {
+      this.state.starStates[num_on] = 1;
     }
   }
 
   /** [is_on] checks whether the [pos+1]th star from the left is on or off. */
   is_on(pos) {
-    if (this.state.star_states[pos] == 1) {
+    if (this.state.starStates[pos] == 1) {
       return true;
     }
     else {
@@ -135,20 +119,14 @@ class Dashboard extends Component {
 
   /** [completion_indicator] returns a string with the name of the task and its status. */
   completion_indicator(task, isComplete) {
-    task = task.replace(/\s/g, ''); // removes all spaces from string [task]
-    if (task === "") { //  if there is no task entered by user
-      return "";
+    progress = "";
+    if (isComplete == 1) {
+      progress = "Complete";
     }
-    else { // if there is a task entered by user
-      progress = "";
-      if (isComplete == 1) {
-        progress = "Complete";
-      }
-      else {
-        progress = "Incomplete";
-      }
-      return task + '  [' + progress + ']';
+    else {
+      progress = "Incomplete";
     }
+    return task + '  [' + progress + ']';
   }
 
   /** [server_retrieve] is a sample function (for testing) that logs a user's response. */
@@ -163,13 +141,13 @@ class Dashboard extends Component {
         <View style={{ justifyContent: 'space-between' }}>
           {this.renderNavigationbar(this.props)}
           <Card
-            title={I18n.t('Dashboard.dashboardTitle')}
+            title={'Course: ' + this.state.course}
             titleStyle={styles.cardTitle}
             containerStyle={{ marginBottom: 15 }}>
-            <Text>Welcome to MobileCoach!</Text>
-            <Text>Below, you will find your progress. Each star corresponds to task completion for a single day.</Text>
-          </Card>
-          <View style={{ flexDirection: 'row', flex: 2 }}>
+            <Text style={styles.TextElement}>Below, you will find your progress. Each star corresponds to task completion for a single day.</Text>
+            <Text></Text><Text></Text>
+
+            <View style={{ flexDirection: 'row', flex: 2 }}>
             <Image
               style={styles.stretch}
               source={(this.state.stars >= 1 ? require('./filled-star.png') : require('./empty-star.png'))}
@@ -199,50 +177,45 @@ class Dashboard extends Component {
               source={(this.state.stars >= 7 ? require('./filled-star.png') : require('./empty-star.png'))}
             />
           </View>
+
           <View>
             <Image
               style={styles.circle}
               source={require('./blue-circle.png')}
             />
             <Text style={styles.numInteraction}>
-              {this.state.proportion_tasks_complete + '%'}
+              {this.state.proportionTasksComplete + '%'}
             </Text>
           </View>
-        </View>
-        <Text/>
-      <View style={styles.subsection}>
-        <Text style={styles.heading}>
+          
+          <View style={styles.subsection}>
+            <Text style={styles.heading}>
             Your Plan
-        </Text>
-        <Text style={styles.bullet}>
-          {this.completion_indicator(this.state.task1, this.state.task1Complete)}
-        </Text>
-        <Text style={styles.bullet}>
-          {this.completion_indicator(this.state.task2, this.state.task2Complete)}
-        </Text>
-        <Text style={styles.bullet}>
-          {this.completion_indicator(this.state.task3, this.state.task3Complete)}
-        </Text>
-        <Text style={styles.bullet}>
-          {this.completion_indicator(this.state.task4, this.state.task4Complete)}
-        </Text>
-        <Text style={styles.bullet}>
-          {this.completion_indicator(this.state.task5, this.state.task5Complete)}
-        </Text>
-      </View>
+            </Text>
+          <View>
+            {
+              this.state.dynamicTasks.map((item,index) => {
+                return <Text style={styles.bullet}>{this.completion_indicator(item, this.state.dynamicTasksCompletion[index])}</Text>
+              })
+            }
+          </View>
+        </View>
 
-      <View style={styles.subsection}>
+        <View style={styles.subsection}>
         <Text style={styles.heading}>
             Your Past Accomplishments
         </Text>
         <Text style={styles.bullet}>
-          Last week: {this.state.last_week} stars
+          Last week: {this.state.lastWeek} stars
         </Text>
         <Text  style={styles.bullet}>
           Highest: {this.state.highest} stars
         </Text>
+        </View>
+
+        </Card>
       </View>
-      </ScrollView>
+    </ScrollView>
     );
   }
 }
@@ -267,15 +240,15 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
   },
   stretch: {
-    width: 55,
-    height: 60,
+    width: 48,
+    height: 50,
     resizeMode: 'stretch',
   },
   circle: {
-    width: 200,
-    height: 200,
+    width: 175,
+    height: 175,
     left: 100,
-    marginTop: 20,
+    marginTop: 40,
     justifyContent: 'center',
   },
   numInteraction: {
@@ -283,8 +256,8 @@ const styles = StyleSheet.create({
     color: "white",
     fontWeight: 'bold',
     fontSize: 50,
-    left: 0,
-    bottom: 130,
+    left: 15,
+    bottom: 120,
   },
   cardTitle: {
     textAlign: 'left',
@@ -292,6 +265,10 @@ const styles = StyleSheet.create({
   },
   content: {
     flex: 1,
+    backgroundColor: "white",
+  },
+  textElement: {
+    fontSize: 16
   },
   input: {
     height: 40,
@@ -306,14 +283,14 @@ const styles = StyleSheet.create({
     marginTop: 5,
     marginLeft: 12,
     textAlign: "left",
-    fontSize: 28,
+    fontSize: 25,
     fontWeight: 'bold'
   },
   bullet: {
     marginTop: 5,
     marginLeft: 12,
     textAlign: "left",
-    fontSize: 18
+    fontSize: 16
   },
   subsection: {
     marginBottom: 20
